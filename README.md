5bbae175ee13d8d11daff82f97604f6f15e054f2
My Name <delanoleslieprof@gmail.com>
2025-09-07 16:02:29 -0700

    First commit, simple functionality

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kitty IPY Mime
+
+This is a simple IPython extension that add the `image/png` mime type to IPython so it can display images in the terminal :)
+
+
---
+++ __init__.py
@@ -0,0 +1,30 @@
+from base64 import encodebytes
+from IPython.lib.latextools import latex_to_png
+import sys
+import subprocess
+
+def mathcat(data, meta):
+    png = latex_to_png(f'$${data}$$'.replace('\\displaystyle', '').replace('$$$', '$$'))
+    imcat(png, meta)
+
+def imcat(image_data, metadata):
+    icat = subprocess.Popen(
+            ['kitty', 'icat'],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        )
+
+    icat.stdin.write(image_data);
+    icat.stdin.close()
+
+    image_kgp_command = icat.stdout.read()
+    sys.stdout.buffer.write(image_kgp_command)
+
+def register_mimerenderer(ipython, mime, handler):
+    ipython.display_formatter.active_types.append(mime)
+    ipython.display_formatter.formatters[mime].enabled = True
+    ipython.mime_renderers[mime] = handler
+
+def load_ipython_extension(ipython):
+    register_mimerenderer(ipython, 'image/png', imcat)
+    register_mimerenderer(ipython, 'text/latex', mathcat)
